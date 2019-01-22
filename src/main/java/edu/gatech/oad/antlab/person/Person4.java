d4d50488b89831b705c1b0133f14ffc67861717f
PIneaPplez13 <pineappleslabs@gmail.com>
2019-01-22 11:38:01 -0500

    Initial Branch Commit woohoo

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -30,8 +30,13 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+      // Person 4 put your implementation here
+      // Okay
+      String n = "";
+      for (int i = 0; i < input.length(); i++) {
+        n += input.charAt(i) + 1;
+      }
+      return n;
     }
     
     /**
